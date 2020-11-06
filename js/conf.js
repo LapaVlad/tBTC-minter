cac599dbd7ab79166fc9af8dadfebeedb1ab86c0
LapaVlad <74074255+LapaVlad@users.noreply.github.com>
2020-11-06 23:16:33 +0300

    Add files via upload

---
+++ js/conf.js
@@ -0,0 +1,24 @@
+let conf = {
+    ethRpcUrl: "https://ropsten.infura.io/v3/a129******4217",
+
+    // make deposit
+    lotSizeBTC: 1,
+
+    // redeem
+    ridderAddress: "m******yuv",
+
+    // send BTC
+    blockcypherApiToken: "ef92f******1d57",
+    network: "testnet", // testnet mainnet
+    wifPrivateKey: "cUP******z",
+    txFee: 0.0001, // btc
+
+    // database
+    mysqlHost: "",
+    mysqlPort: 3306,
+    mysqlUserName: "",
+    mysqlPassword: "",
+    mysqlDatabase: ""
+};
+
+export default conf
